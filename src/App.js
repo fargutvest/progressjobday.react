9b26897aa5c47dcdc5863942225c0daea1cccfaf
Farragut West <fargutvest@gmail.com>
2023-10-15 14:57:26 +0300

    tick every second

--- src/App.js
+++ src/App.js
@@ -1,40 +1,60 @@
 import './App.css';
 import Progress from 'react-progressbar';
+import React, { useState, useEffect } from 'react';
+
+function twoDigits(n){
+  return n > 9 ? "" + n: "0" + n;
+}
+
+function totalSecondsToTimeString(totalSeconds){
+  
+  let hours = Math.floor(totalSeconds / 3600);
+  let minutes = Math.floor((totalSeconds - hours * 3600) / 60);
+  let seconds = Math.floor((totalSeconds - hours * 3600 - minutes * 60));
+  
+  return twoDigits(hours) + " ч  " + twoDigits(minutes) + " м  " + twoDigits(seconds) + " c";
+}
 
 function App() {
 
-  const currentDate = new Date();
-  const hours = currentDate.getHours();
-  const minutes = currentDate.getMinutes();
-
+  const [time, setTime] = useState(new Date());
+  
   const startWorkDayHour = 9;
-  const totalWorkingMinutes = 540;
-
-  let totalWorkedMinutes = hours * 60 + minutes - startWorkDayHour * 60;
-
-  let totalRemainedMinutes = totalWorkingMinutes - totalWorkedMinutes;
+  const workDayDurationInHours = 9;
 
   let percent = "";
   let elapsedCalc = "";
   let toHomePredict = "";
 
 
-  if (totalWorkedMinutes < 0) {
+  if (time.getHours() < startWorkDayHour) {
     percent = 0;
     elapsedCalc = toHomePredict = "рабочий день еще не начался";
   }
   else {
-    percent = totalWorkedMinutes / totalWorkingMinutes * 100;
+    let totalSeconds = time.getHours() * 3600 + time.getMinutes() * 60 + time.getSeconds();
+    let totalWorkedSeconds = totalSeconds - startWorkDayHour * 3600;
+    percent = totalWorkedSeconds / (workDayDurationInHours * 3600) * 100;
+
     if (percent > 100)
       percent = 100;
 
-    elapsedCalc = Math.floor(totalWorkedMinutes / 60) + " ч " + totalWorkedMinutes % 60 + "м";
-    if (totalRemainedMinutes <= 0)
+    elapsedCalc = totalSecondsToTimeString(totalWorkedSeconds);
+     
+    let totalRemainedSeconds = workDayDurationInHours * 3600 - totalWorkedSeconds;
+    
+    if (totalRemainedSeconds <= 0)
       toHomePredict = "уже пора";
     else
-      toHomePredict = Math.floor(totalRemainedMinutes / 60) + " ч  " + totalRemainedMinutes % 60 + "м";
+      toHomePredict = totalSecondsToTimeString(totalRemainedSeconds);
   }
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div>
